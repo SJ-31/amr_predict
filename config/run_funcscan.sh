c231fe9a4af4854194d22bead29e0ad11a45ae00
Shann Chongwattanankul <shanj3131@gmail.com>
2025-09-23 09:08:58 +0700

    chore: add nextflow configurations for funcscan

---
+++ config/run_funcscan.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# [2025-09-22 Mon] Keep it using the jia samples for now
+# Run from repos/amr_predict/pipelines
+remote=".."
+nextflow run nf-core/funcscan \
+    --input "./jia_samplesheet.csv" \
+    --outdir "${remote}/output/jia_funcscan_2025-09-22" \
+    -profile singularity \
+    --run_arg_screening \
+    --annotation_tool bakta \
+    --annotation_bakta_db "${remote}/datasets/bakta_db_v5.1_full" \
+    --save_annotations true \
+    --run_protein_annotation
