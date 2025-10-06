df53583b290230d0bf9456af73dd5e34e2aa0914
Shann Chongwattanankul <shanj3131@gmail.com>
2025-10-06 11:17:44 +0700

    chore: add config files for new genome set

---
+++ config/run_bacass.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+remote=".."
+nextflow run nf-core/bacass \
+    --input ~/amr_predict/config/moradigaravand_samplesheet.csv \
+    --kraken2db "/data/project/stemcell/shannc/repos/amr_predict/cache/k2_standard_16_GB_20250714.tar.gz" \
+    --ncbi_assembly_metadata "/data/project/stemcell/shannc/repos/amr_predict/cache/assembly_summary_refseq_2025-10-06.txt" \
+    --kmerfinderdb "/data/project/stemcell/shannc/repos/amr_predict/cache/20190108_kmerfinder_stable_dirs.tar.gz" \
+    --outdir "${remote}/output/moradigaravand_2025-10-06" \
+    -profile singularity \
+    --skip_annotation \
+    -c "./nf_config_fix.config"
