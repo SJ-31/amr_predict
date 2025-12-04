ad9e7974437667807d6bbf248cdd0248f60c721d
Shann Chongwattanankul <shanj3131@gmail.com>
2025-12-04 11:34:59 +0700

    chore: get reference taxids

--- docs/view_dataset.py
+++ docs/view_dataset.py
@@ -9,11 +9,13 @@ def _():
     import json
     from pathlib import Path
 
+    import marimo as mo
     import polars as pl
+    import polars.selectors as cs
     from pyhere import here
 
     META = here("data", "meta")
-    return META, here, pl
+    return META, cs, here, mo, pl
 
 
 @app.cell
@@ -72,7 +74,7 @@ def _(bprojects):
 
 @app.cell
 def _(mo):
-    mo.md("## Filter appropriate genomes to download")
+    mo.md("""## Filter appropriate genomes to download""")
     return
 
 
@@ -99,8 +101,8 @@ def _(here, pl, tb: "pl.DataFrame"):
 def _(filtered, here, pl, tb: "pl.DataFrame"):
     to_assemble = tb.filter(~pl.col("BioSample").is_in(filtered["BioSample"]))
     to_assemble_file = here("data", "temp", "ast_to_assemble.txt")
-    to_assemble_file.write_text("\n".join(to_assemble["BioSample"]))
-    return
+    to_assemble_file.write_text("\n".join(to_assemble["Run"]))
+    return (to_assemble,)
 
 
 @app.cell
@@ -109,9 +111,78 @@ def _(mo):
     return
 
 
+@app.cell(hide_code=True)
+def _(cs, pl, taxids, to_assemble):
+    required_id = taxids.filter(
+        pl.col("taxid_query").is_in(to_assemble["TaxID"])
+    ).filter(pl.col("ANI Check status") == "OK")
+    ani_status_scoring = {
+        "species_match": 1.2,
+        "synonym_match": 1.2,
+        "derived_species_match": 1.5,  # At the subspecies level,
+        # but could a different subspecies to the submitted
+        "subspecies_match": 2.0,
+        "below_threshold_mismatch": 1.0,
+    }
+    level_scoring = {
+        "Chromosome": 1.2,
+        "Complete Genome": 1.3,
+        "Scaffold": 1.1,
+        "Contig": 1.0,
+    }
+    required_id = (
+        required_id.with_columns(
+            pl.col("ANI Best match status")
+            .replace(ani_status_scoring)
+            .alias("ani_match_score"),
+            pl.col("Assembly Refseq Category")
+            .replace({None: 1.0, "reference genome": 1.5})
+            .alias("refseq_category_score"),
+            pl.col("Assembly Level")
+            .replace(level_scoring)
+            .alias("assembly_level_score"),
+        )
+        .cast({cs.ends_with("_score"): pl.Float64})
+        .with_columns(
+            (
+                pl.col("ani_match_score")
+                * pl.col("refseq_category_score")
+                * pl.col("assembly_level_score")
+                * pl.col("ANI Best ANI match ANI")
+            ).alias("score")
+        )
+    )
+    required_id
+    return (required_id,)
+
+
 @app.cell
-def _(taxids):
-    taxids
+def _(pl, required_id):
+    dfs = []
+    per_group = 3
+    for taxid, group in required_id.group_by("taxid_query"):
+        print(f"{taxid=}")
+        group = group.filter(pl.col("Assembly Atypical Is Atypical").is_null()).unique(
+            "Assembly Accession"
+        )
+        print(group.shape)
+        nrow, _ = group.shape
+        if nrow <= per_group:
+            dfs.append(group)
+            continue
+        top = group.top_k(per_group, by="score")
+        print(top)
+        dfs.append(top)
+    return (dfs,)
+
+
+@app.cell
+def _(META, dfs, here, pl):
+    ref_df = pl.concat(dfs)
+    ref_df.write_csv(here(META, "reference_genomes.tsv"), separator="\t")
+    here("data", "temp", "reference_accs.txt").write_text(
+        "\n".join("Assembly Accession")
+    )
     return
 
 
