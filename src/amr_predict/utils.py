c34f5f3872d8554152e71b447ce6da8a68fc66a3
Shann Chongwattanankul <shanj3131@gmail.com>
2025-12-12 10:18:06 +0700

    feat: add function mapping torch to polars types

--- src/amr_predict/utils.py
+++ src/amr_predict/utils.py
@@ -820,6 +820,37 @@ def features_from_df(
     return converted
 
 
+def torch2pl(
+    dtype: torch.dtype | Sequence[torch.dtype],
+) -> pl.DataType | list[pl.DataType]:
+    # Valid v2.9.1
+    mapping: dict = {
+        torch.bool: pl.Boolean,
+        torch.int8: pl.Int8,
+        torch.uint8: pl.UInt8,
+        torch.int16: pl.Int16,  # alias: torch.short
+        torch.uint16: pl.UInt16,
+        torch.int32: pl.Int32,  # alias: torch.int
+        torch.uint32: pl.UInt32,
+        torch.int64: pl.Int64,  # alias: torch.long
+        torch.uint64: pl.UInt64,
+        torch.float32: pl.Float32,  # alias: torch.float
+        torch.float64: pl.Float64,  # alias: torch.double
+    }
+    if isinstance(dtype, Sequence):
+        converted = []
+        for tp in dtype:
+            try:
+                converted.append(mapping[tp])
+            except KeyError:
+                raise ValueError(f"`{tp}` is not supported by polars")
+        return converted
+    try:
+        return mapping[dtype]
+    except KeyError:
+        raise ValueError(f"`{dtype}` is not supported by polars")
+
+
 def torch2hf(dtype: torch.dtype | Sequence[torch.dtype]) -> Value | list[Value]:
     # Valid v2.9.1
     mapping: dict = {
