c5bd91cf4d00901b112f684d7cdd38dca49d990d
Shann Chongwattanankul <shanj3131@gmail.com>
2025-12-12 14:54:41 +0700

    chore: update bacass run to new version

--- config/nf_runs/bacass_ast.sh
+++ config/nf_runs/bacass_ast.sh
@@ -6,10 +6,12 @@ nextflow run -r master nf-core/bacass \
     -resume \
     --input ~/amr_predict/config/nf_runs/ast_browser_samplesheet_2025-12-05.csv \
     --kraken2db "/data/project/stemcell/shannc/repos/amr_predict/cache/k2_standard_16_GB_20250714.tar.gz" \
-    --ncbi_assembly_metadata "/data/project/stemcell/shannc/repos/amr_predict/cache/assembly_summary_refseq_2025-10-06.txt" \
     --kmerfinderdb "/data/project/stemcell/shannc/repos/amr_predict/cache/20190108_kmerfinder_stable_dirs.tar.gz" \
     --outdir "${remote}/output/ast_browser/bacass_2025-12-05" \
     -profile singularity \
     --skip_annotation \
     --skip_kmerfinder \
+    --assembly_type "short" \
     -c "./nf_config_fix.config"
+
+# --ncbi_assembly_metadata "/data/project/stemcell/shannc/repos/amr_predict/cache/assembly_summary_refseq_2025-10-06.txt" \ [2025-12-11 Thu] This flag is not used in latest version
