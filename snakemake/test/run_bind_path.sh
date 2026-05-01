96b48faaba0734d3c5b11dbfc590c19f806ffd2e
Shann Chongwattanankul <shanj3131@gmail.com>
2026-05-01 11:49:44 +0700

    chore: misc. env updates

--- snakemake/test/run_bind_path.sh
+++ snakemake/test/run_bind_path.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 snakemake --use-singularity \
-    --singularity-args "--nv --bind $HOME/amr_predict/src:/py_lib --bind /data/project/stemcell/shannc/repos:$PWD/../../data/remote"
+    --singularity-args "--nv --bind $HOME/amr_predict/src:/py_lib --bind /data/project/stemcell/shannc/repos:$PWD/../../data/remote" "$@"
