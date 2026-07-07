29eada5e45caaf1492fe272caf2f04f807dfd2ec
Shann Chongwattanankul <shanj3131@gmail.com>
2026-07-07 15:45:16 +0700

    chore: add caching fn for arbitrary repeats

--- src/amr_predict/cache.py
+++ src/amr_predict/cache.py
@@ -954,3 +954,41 @@ class NamedCache:
         writer(file, obj)
         assert file.exists(), f"Writer function did not write to {file}"
         return obj
+
+
+def with_repeat_caching(
+    cache_dir: Path | str,
+    n: int,
+    writer: Callable[[int, Path], None],
+    reader: Callable,
+    combine: Callable[[list[Any]], None],
+    suffix: str = ".csv",
+    **kws,
+) -> Any:
+    """Helper function to cache the results of a expensive function
+    that involves aribtrary repeats e.g. a bootstrap procedure
+
+    Parameters
+    ----------
+    cache_dir : Path
+        Path to save results of each round
+    writer : Callable
+        Function taking iteration round and filename as input. Must
+        write to the given file
+    reader : Callable
+        Function used to read cached results
+    combine : Callable
+        Function to combine cached results after reading them with `reader`.
+        e.g. pl.concat
+    """
+    if not cache_dir.exists():
+        cache_dir.mkdir(parents=True)
+    n_previous = len(list(cache_dir.glob(f"*{suffix}")))
+    n_remaining: int = n - n_previous
+    for i in range(n_remaining):
+        i = i + n_previous
+        to_cache = cache_dir / f"{i}{suffix}"
+        writer(i, to_cache)
+        assert to_cache.exists(), "Writer function did not write file"
+    result = combine([reader(f) for f in cache_dir.glob(f"*{suffix}")])
+    return result
