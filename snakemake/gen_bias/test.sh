43dace0cb978ea9c0fec1e8a0fbd303d669e128f
Shann Chongwattanankul <shanj3131@gmail.com>
2026-09-23 16:08:02 +0700

    chore: add HF and update test script

--- snakemake/gen_bias/test.sh
+++ snakemake/gen_bias/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-snakemake --configfile test_env.yaml --profile profiles/test/config.yaml
+export HF_HOME="/data/project/stemcell/shannc/repos/amr_predict/cache/huggingface"
+snakemake --use-singularity \
+	--singularity-args "--nv --bind ../../src:/py_lib --bind /data/project:/data/project" \
+	--configfile test_env.yaml --profile profiles/test/config.yaml \
+	"$@"
