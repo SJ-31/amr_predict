a9394057d9be4f81aaa681aefdfca6e307be3a34
Shann Chongwattanankul <shanj3131@gmail.com>
2026-09-24 10:23:21 +0700

    chore: add new build files

--- snakemake/gen_bias/test.sh
+++ snakemake/gen_bias/test.sh
@@ -3,6 +3,6 @@
 export HF_HOME="/data/project/stemcell/shannc/repos/amr_predict/cache/huggingface"
 snakemake --use-singularity \
 	--use-conda \
-	--singularity-args "--nv --bind ../../src:/py_lib --bind /data/project:/data/project" \
+	--singularity-args "--nv --bind ../../src:/py_lib --bind /data/project:/data/project --cleanenv" \
 	--configfile test_env.yaml --profile profiles/test/config.yaml \
 	"$@"
